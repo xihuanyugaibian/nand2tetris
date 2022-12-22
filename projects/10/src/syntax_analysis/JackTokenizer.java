6bc3b7f528db36ccccefd5634638aae4175e6060
fyk <1282230766@qq.com>
2022-12-22 23:55:41 +0800

    第十章 语法分析 词法分析 字元化中

--- projects/10/src/syntax_analysis/JackTokenizer.java
+++ projects/10/src/syntax_analysis/JackTokenizer.java
@@ -1,10 +1,17 @@
 package syntax_analysis;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * 字元转换器模块
  */
 public class JackTokenizer {
-    private static class Const {
+    private static class Constant {
+        private static final String SPACE = " ";
         private static final String KEYWORD = "KEYWORD";
         private static final String SYMBOL = "SYMBOL";
         private static final String IDENTIFIER = "IDENTIFIER";
@@ -33,4 +40,31 @@ public class JackTokenizer {
         private static final String THIS = "THIS";
     }
 
+    public JackTokenizer(InputStream inputStream) throws IOException {
+        StringBuilder sb = new StringBuilder("");
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        //把流中的数据 串成字符串
+        int read;
+        while ((read = inputStreamReader.read()) != -1) {
+            sb.append((char) read);
+        }
+        //去掉字符串中的注释
+        while (sb.indexOf("//") != -1) {
+            int start = sb.indexOf("//");
+            int end = sb.indexOf("\n", start) + "\n".length();
+            sb.delete(start, end);
+        }
+        while (sb.indexOf("/**") != -1) {
+            int start = sb.indexOf("/**");
+            int end = sb.indexOf("*/", start) + "*/".length();
+            sb.delete(start, end);
+        }
+        String[] split = sb.toString().split(Constant.SPACE);
+
+
+        String s = sb.toString();
+        System.out.println(s);
+
+
+    }
 }
